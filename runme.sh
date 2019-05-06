8e6fd2fcf2a0521f28598db81595dd56af58f09b
ovassa <sami.ovaskainen@f-secure.com>
2019-05-06 16:30:27 +0300

    a

--- runme.sh
+++ runme.sh
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 docker build -t test:test .
-
 docker run --name sami -p 80:80 test:test
 
 docker rm sami
 docker image rm test:test
 docker image rm nginx
-=======
-docker build -t test:test .
-docker run --name sami -p 80:80 test:test
-docker rm sami
-docker image rm test:test
-docker image rm nginx
->>>>>>> 02cb14ec381c703f2540819475239aa7ea8fd09f
